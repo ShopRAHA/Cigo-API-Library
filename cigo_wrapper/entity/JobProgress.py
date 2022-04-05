a3a4a8fdd61e45b7d0010e8b1aac007c44e0b91e
Awais Jibran <awaisdar001@gmail.com>
2022-04-06 01:06:56 +0500

    adding to_json methods adding job route class use JobRoute class in VehicleTracking.py

--- cigo_wrapper/entity/JobProgress.py
+++ cigo_wrapper/entity/JobProgress.py
@@ -1,3 +1,6 @@
+import json
+
+
 class JobProgress:
     duration = None
     start_datetime = None
@@ -5,20 +8,23 @@ class JobProgress:
     started = None
     finished = None
 
+    def to_json(self):
+        return json.loads(json.dumps(self, default=lambda o: o.__dict__))
+
     @classmethod
     def from_json(cls, job_response):
         progress = cls()
-        response_dic = job_response
-        for key in response_dic.keys():
-            if key == 'duration' and response_dic[key] is not None:
-                progress.duration = response_dic[key]
-            elif key == 'start_datetime' and response_dic[key] is not None:
-                progress.start_datetime = response_dic[key]
-            elif key == 'end_datetime' and response_dic[key] is not None:
-                progress.end_datetime = response_dic[key]
-            elif key == 'started' and response_dic[key] is not None:
-                progress.started = response_dic[key]
-            elif key == 'finished' and response_dic[key] is not None:
-                progress.finished = response_dic[key]
+        response = job_response
+        for key in response.keys():
+            if key == 'duration' and response[key] is not None:
+                progress.duration = response[key]
+            elif key == 'start_datetime' and response[key] is not None:
+                progress.start_datetime = response[key]
+            elif key == 'end_datetime' and response[key] is not None:
+                progress.end_datetime = response[key]
+            elif key == 'started' and response[key] is not None:
+                progress.started = response[key]
+            elif key == 'finished' and response[key] is not None:
+                progress.finished = response[key]
 
         return progress
