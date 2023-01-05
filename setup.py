a8faff0c9c06f31b139d4f9b226b2beb3085d683
Awais Jibran <awaisdar001@gmail.com>
2023-01-05 21:42:49 +0500

    feat(itinerary): add delete stop endpoint (#3)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Cigo-API-Library',
-    version='0.1.2',
+    version='0.1.3',
     packages=['cigo_wrapper', 'cigo_wrapper.entity', 'cigo_wrapper.network'],
     url='',
     license='MIT License',
